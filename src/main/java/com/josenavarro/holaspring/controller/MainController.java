7d2b47d93e746c898e003a9cff52efd93b5e882f
Jose <eljosele4@gmail.com>
2020-02-10 10:16:56 +0100

    explicación parámetros

--- src/main/java/com/josenavarro/holaspring/controller/MainController.java
+++ src/main/java/com/josenavarro/holaspring/controller/MainController.java
@@ -2,6 +2,7 @@ package com.josenavarro.holaspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController{
     @GetMapping("/") // Entradas en la tabla de rutas solo una por dirección, ejecuta el método de abajo al encontrar /
     @ResponseBody // Busca plantilla timelift que responde el nombre que le pongamos
-    public String holaMundo(){
-        return "Hola buenos dias";
+    public String holaMundo(@RequestParam("nombre") String name){ /** Request es la petición y Response la respuesta, lo captura y lo guarad en el String name */
+        return "Hola "+name;
     }
     
     @GetMapping("/ingles") // Entradas en la tabla de rutas solo una por dirección
@@ -21,5 +22,15 @@ public class MainController{
     public String holaMundoE(){
         return "Hello World";
     }
+
+    /**Los navegadores usan GET: traer páginas web, POST: rellenamos formulario, PUT, DELETE y OPTIONS
+     * GET en la dirección y POST van ocultos (passwords)
+     * http://localhost:8080/?param=pepe&param2=juan
+     * llamada a localhost:8080 pero pasando dos parámetros
+     * http://localhost:8080/?nombre=pepe
+     * 
+     * 
+    */
+
 
 }
