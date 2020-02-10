87a989ca1d085a6987de25316508d774e6153e3d
Jose <eljosele4@gmail.com>
2020-02-10 12:21:55 +0100

    Hacer lo mismo pero con templates hello

--- src/main/java/com/josenavarro/holaspring/controller/MainController.java
+++ src/main/java/com/josenavarro/holaspring/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * MainController
@@ -12,16 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController{
     @GetMapping("/") // Entradas en la tabla de rutas solo una por dirección, ejecuta el método de abajo al encontrar "/" es la ruta servidor
-    @ResponseBody // Busca plantilla timelift que responde el nombre que le pongamos
+    @ResponseBody // Busca plantilla timeleaf que responde el nombre que le pongamos
     public String holaMundo(@RequestParam("nombre") String name, @RequestParam("edad") Integer edad){ /** Request es la petición y Response la respuesta, lo captura y lo guarad en el String name */
         return "Hola "+name+", edad "+edad; // http://localhost:8080/?nombre=Jose&edad=36
     }
     
     @GetMapping("/ingles") // Entradas en la tabla de rutas solo una por dirección
-    @ResponseBody // Busca plantilla timelift que responde el nombre que le pongamos
+    @ResponseBody // Busca plantilla timeleaf que responde el nombre que le pongamos
     public String holaMundoE(){
         return "Hello World";
     }
+
 
     /**Los navegadores usan GET: traer páginas web, POST: rellenamos formulario, PUT, DELETE y OPTIONS
      * GET en la dirección y POST van ocultos (passwords)
@@ -32,5 +34,23 @@ public class MainController{
      * 
     */
 
+    /**Qué es lo que hace timeleef
+     * La llamada GET desde el navegador va al controlador (controller), y spring va a usar la vista
+     * y esta te genere el html para devolver, y este html será enviado. Esta vista en timeleaf hace 
+     * plantillas en HTML para ver cómo es la vista.
+     * En resources spring boot lo gestiona por defecto, static(css, javascript, fotos, etc) y templates:
+     * donde va a guardar las plantillas de timeleaf
+     * 
+     */
+
+     
+    //Devolver página con templates
+    @GetMapping("/nuevo")
+    public ModelAndView helloHTML(){
+        ModelAndView respuesta = new ModelAndView("hello");//nombre del template
+        String msg = "hola mundo html";
+        respuesta.addObject("mensaje", msg);
+        return respuesta;
+    }
 
 }
